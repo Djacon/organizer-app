4c03ec70d0c990adb9b8fa018a6d6ae04bbeddd5
Djacon <danielille100@gmail.com>
2024-01-21 10:15:28 +0300

    add todo panel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,11 +4,11 @@ window.addEventListener('beforeunload', (event) => {
     return ''; 
 });
 
-blocks = document.getElementsByClassName('task-block');
+const blocks = document.getElementsByClassName('task-block');
 
 for (let block of blocks) {
     checkbox = block.children[0];
-    checkbox.addEventListener("click", function() {
+    checkbox.addEventListener('click', function() {
         if (this.classList.contains('active-btn')) {
             this.classList.remove('active-btn');
             block.children[1].classList.remove('finished-task');
@@ -16,5 +16,22 @@ for (let block of blocks) {
             this.classList.add('active-btn');
             block.children[1].classList.add('finished-task');
         }
-    })
+    });
+}
+
+const maxWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+// Handle hidden panel only on mobile version (yet)
+if (maxWidth < 991) {
+    const task_adder = document.getElementById('new-task-section');
+
+    const task_adder_btn = document.getElementById('add-task');
+    task_adder_btn.addEventListener('click', function() {
+        task_adder.classList.add('show-section');
+    });
+
+    const task_close_btn = document.getElementById('cancel-btn');
+    task_close_btn.addEventListener('click', function() {
+        task_adder.classList.remove('show-section');
+    });
 }
